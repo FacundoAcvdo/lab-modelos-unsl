848c38c2cafee0885e94992b1770f2dbba915f9d
Emma <fernandezemmanuel534@gmail.com>
2024-04-14 19:15:35 -0300

    Randomizer prueba está más lindo

--- src/main/java/org/example/RandomizerPruebas.java
+++ src/main/java/org/example/RandomizerPruebas.java
@@ -1,30 +1,13 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class RandomizerPruebas implements Randomizer{
     private ArrayList<Double> randoms;
 
     public RandomizerPruebas() {
-        this.randoms = new ArrayList<Double>();
-        this.randoms.add(0.2);
-        this.randoms.add(0.98);
-        this.randoms.add(0.5);
-        this.randoms.add(0.5);
-        this.randoms.add(0.2);
-        this.randoms.add(0.5);
-        this.randoms.add(0.5);
-        this.randoms.add(0.5);
-        this.randoms.add(0.5);
-        this.randoms.add(0.5);
-        this.randoms.add(0.5);
-        this.randoms.add(0.5);
-        this.randoms.add(0.5);
-        this.randoms.add(0.5);
-        this.randoms.add(0.1);
-        this.randoms.add(0.1);
-        this.randoms.add(0.1);
-        this.randoms.add(0.1);
+        this.randoms = new ArrayList<Double>(Arrays.asList(0.2,0.98,0.5,0.5,0.2,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.1,0.1,0.1,0.1));
     }
     public double nextDouble() {
         double retorno = randoms.getFirst();
